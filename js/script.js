266b02574ffd185c80299535fba9e4a435f4921e
DennisOnder <webdev.dennisonder@gmail.com>
2019-03-20 01:48:19 +0100

    JS scripts

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
-"use strict";
+const redirectToAbout = () => {
+  window.location.href = '/about';
+};
 
-console.log('Test');
+const navTagUnderline = () => {
+  const tags = document.getElementsByClassName('nav_link');
+  const urlLocation = `/${window.location.href.split('/')[3].replace('.html', '')}`;
+  // Remove class from all tags
+  for (let i = 0; i < tags.length; i++) {
+    tags[i].classList.remove('nav_link--active');
+  }
+  // Check if the page is on the landing page
+  if (urlLocation === '/index') {
+    tags[0].classList.add('nav_link--active');
+  } else {
+    for (let i = 0; i < tags.length; i++) {
+      if (tags[i].getAttribute('href') === urlLocation) {
+        tags[i].classList.add('nav_link--active');
+      }
+    }
+  }
+};
+
+navTagUnderline();
+document.getElementById('learnMore').addEventListener('click', redirectToAbout);
