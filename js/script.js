6e25b9840b9d265cd176015a024342f981f9ac9a
DennisOnder <webdev.dennisonder@gmail.com>
2019-03-20 02:57:34 +0100

    HTML templating, script tweaking

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const _redirectToHome = () => {
 
 const navTagUnderline = () => {
   const tags = document.getElementsByClassName('nav_link');
-  const urlLocation = `/${window.location.href.split('/')[3].replace('.html', '')}`;
+  const urlLocation = `/${window.location.href.split('/')[3]}`;
   // Remove class from all tags
   for (let i = 0; i < tags.length; i++) {
     tags[i].classList.remove('nav_link--active');
@@ -18,7 +18,8 @@ const navTagUnderline = () => {
     tags[0].classList.add('nav_link--active');
   } else {
     for (let i = 0; i < tags.length; i++) {
-      if (tags[i].getAttribute('href') === urlLocation) {
+      const attrLocation = tags[i].getAttribute('href').replace('.', '');
+      if (attrLocation === urlLocation) {
         tags[i].classList.add('nav_link--active');
       }
     }
@@ -26,5 +27,8 @@ const navTagUnderline = () => {
 };
 
 navTagUnderline();
-document.getElementById('learnMore').addEventListener('click', _redirectToAbout);
 document.getElementById('navLogo').addEventListener('click', _redirectToHome);
+
+if (document.getElementById('learnMore')) {
+  document.getElementById('learnMore').addEventListener('click', _redirectToAbout);
+}
