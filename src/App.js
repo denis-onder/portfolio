385117b6acef6c71a9aeb84c3e80befe1738db0b
Denis Onder <denis_onder@protonmail.com>
2019-11-12 23:10:30 +0100

    Moved nav component to the scope of the router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Landing from "./components/landing";
 import About from "./components/about";
 import Projects from "./components/projects";
 import Contact from "./components/contact";
-import Nav from "./components/layout/nav";
 
 function App() {
   return (
-    <React.Fragment>
-      <Nav />
-      <Router>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/projects" component={Projects} />
-        <Route exact path="/contact" component={Contact} />
-      </Router>
-    </React.Fragment>
+    <Router>
+      <nav id="nav">
+        <div className="nav_logo" id="navLogo">
+          <Link to="/">
+            <p>
+              d<span>o</span>
+            </p>
+          </Link>
+        </div>
+        <ul className="nav_links">
+          <li>
+            <Link to="/" className="nav_link">
+              home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="nav_link">
+              about me
+            </Link>
+          </li>
+          <li>
+            <Link to="/projects" className="nav_link">
+              projects
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className="nav_link">
+              contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Route exact path="/" component={Landing} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/projects" component={Projects} />
+      <Route exact path="/contact" component={Contact} />
+    </Router>
   );
 }
 
