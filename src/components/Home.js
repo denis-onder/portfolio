ac42b298dd5623aa15c7794edc9286a92ff6ce05
Denis Onder <denis_onder@protonmail.com>
2019-12-31 14:01:54 +0100

    WIP reminder box

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,33 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../scss/home.scss";
 
+// Show and hide info tag
+function revealAndCloseInfo() {
+  const info = document.getElementById("info");
+  // Revealing function
+  function reveal() {
+    // Remove the class, set the opacity back to default and then reveal the box by adding the 'reveal' class
+    info.classList.remove("reveal");
+    info.style.opacity = "1";
+    info.classList.add("reveal");
+    setTimeout(close, 3000);
+  }
+  // Closing function
+  function close() {
+    info.style.opacity = "0";
+  }
+  // Code goes here
+  reveal();
+}
+
+window.onload = revealAndCloseInfo;
+
 export default function Home() {
   return (
     <div id="home">
+      <div id="info">
+        Please note that this project is still <b>WIP</b>. Thanks!
+      </div>
       <h1 className="home_name">
         denis <span>onder</span>
       </h1>
