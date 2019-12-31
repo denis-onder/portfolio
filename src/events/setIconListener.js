0a6038d9c3a83e393037c8b76c1b9c63df69e778
Denis Onder <denis_onder@protonmail.com>
2019-12-31 14:46:34 +0100

    Removed icon listener for now

--- src/events/setIconListener.js
+++
@@ -1,8 +0,0 @@
-export default () => {
-  const links = Array.from(
-    document.getElementsByClassName("navigation_icons_icon--link")
-  );
-  links.forEach(link =>
-    link.addEventListener("click", () => console.log(link))
-  );
-};
