79df62c19d2bf50079c8a5b3e9019c23e09e3968
Denis Onder <denis_onder@protonmail.com>
2020-01-03 22:59:03 +0100

    SCSS

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "../scss/contact.scss";
 
 export default function Contact() {
   return (
-    <div id="contact">
+    <div id="contact" className="margins">
       <h1>contact</h1>
     </div>
   );
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "../scss/portfolio.scss";
 
 export default function Portfolio() {
   return (
-    <div id="portfolio">
+    <div id="portfolio" className="margins">
       <h1>portfolio</h1>
     </div>
   );
