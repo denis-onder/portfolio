e35afa25927b1c04840185f9f07f7c4c968bbb9f
Denis Onder <denis_onder@protonmail.com>
2020-01-05 14:53:02 +0100

    Basic contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "../scss/contact.scss";
 
 export default function Contact() {
+  const [email, setEmail] = useState("test");
   return (
-    <div id="contact" className="margins">
-      <h1>contact</h1>
+    <div id="contact">
+      <div className="contact_form">
+        <h1>contact</h1>
+        <form onSubmit={e => console.log(e)}>
+          <input type="text" placeholder="name: " />
+          <input type="text" placeholder="email: " />
+          <textarea rows="15" placeholder="message: "></textarea>
+          <button
+            type="submit"
+            onClick={e => {
+              e.preventDefault();
+              console.log(email);
+            }}
+          >
+            send
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
