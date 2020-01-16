4003eeca2d8b35ec5e7b54a49ac4e76c9c93dadb
Denis Onder <denis_onder@protonmail.com>
2020-01-16 23:55:16 +0100

    Particles

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,57 @@ import { Link } from "react-router-dom";
 import "../scss/home.scss";
 import setCurrentlyActiveIcon from "../events/setCurrentlyActiveIcon";
 import setTitle from "../events/setTitle";
+import Particles from "react-particles-js";
 
 export default function Home() {
   setTitle("home");
   return (
     <div id="home">
+      <Particles
+        params={{
+          particles: {
+            number: {
+              value: 60,
+              density: {
+                enable: true,
+                value_area: 1500
+              }
+            },
+            line_linked: {
+              enable: true,
+              opacity: 0.02
+            },
+            move: {
+              direction: "right",
+              speed: 0.05
+            },
+            size: {
+              value: 1
+            },
+            opacity: {
+              anim: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0.05
+              }
+            }
+          },
+          interactivity: {
+            events: {
+              onclick: {
+                enable: true,
+                mode: "push"
+              }
+            },
+            modes: {
+              push: {
+                particles_nb: 1
+              }
+            }
+          },
+          retina_detect: true
+        }}
+      />
       <h1 className="home_name">
         denis <span>onder</span>
       </h1>
