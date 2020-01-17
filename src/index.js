946ffcffbf78a6897a775cf90d544ccde503ecfc
Denis Onder <denis_onder@protonmail.com>
2020-01-17 01:27:42 +0100

    Removed the files. They didn't help a bit.

--- src/index.js
+++ src/index.js
@@ -13,16 +13,6 @@ window.onload = () => {
   document.getElementById(window.location.pathname).classList.add("active");
 };
 
-// FIXME Work around this solution. This is in place so Netlify can redirect the user back to home if it does not render out the correct route.
-document.onreadystatechange = () => {
-  if (
-    // eslint-disable-next-line
-    process.env.NODE_ENV !== "development" &&
-    window.location.pathname !== "/"
-  )
-    window.location.href = "/";
-};
-
 ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.unregister();
